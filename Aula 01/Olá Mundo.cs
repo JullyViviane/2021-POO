06d1e69cd4dc2dc6b9b3d678a630bf29f10ebc47
JullyViviane <47670952+JullyViviane@users.noreply.github.com>
2021-01-18 19:48:26 -0300

    Create Olá Mundo.cs

---
+++ Aula 01/Olá Mundo.cs
@@ -0,0 +1,8 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Meu nome é Jully Viviane da Silva");
+    Console.WriteLine("Minha matrícula é 20191011110015");
+  }
+}
